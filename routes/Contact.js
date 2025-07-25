537ffab87136b32e98b4e7059fa1cfa8026c853d
veer2238 <114563813+veer2238@users.noreply.github.com>
2025-07-25 09:23:11 +0530

    Update Contact.js

--- routes/Contact.js
+++ routes/Contact.js
@@ -28,7 +28,9 @@ app.post("/contact", async (req, res) => {
   
     
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
