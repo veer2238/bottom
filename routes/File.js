177ca784070003275f9f162e1006b23af30d5bc8
veer2238 <114563813+veer2238@users.noreply.github.com>
2025-07-25 09:22:58 +0530

    Update File.js

--- routes/File.js
+++ routes/File.js
@@ -65,10 +65,10 @@ app.post('/upload', async (req, res) => {
   
   
       
-  
-      
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
+  const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
